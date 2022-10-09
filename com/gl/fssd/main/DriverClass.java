ac95a89dc25a4bbee679e0832aa19b7caa33c66b
Ganesh Kumar B <ganesh.vinu@gmail.com>
2022-10-09 13:03:47 +0530

    Update DriverClass.java

--- com/gl/fssd/main/DriverClass.java
+++ com/gl/fssd/main/DriverClass.java
@@ -7,24 +7,27 @@ public class DriverClass {
     public static void main(String[] args) {
         System.out.println("\n -------------------------------\n"); 
         AdminDepartment admin = new AdminDepartment();
-        System.out.println(admin.departmentName());
+        System.out.println(" Welcome to"+ admin.departmentName());
         System.out.println(admin.getTodaysWork());
         System.out.println(admin.getWorkDeadline());
+        System.out.println(admin.isTodayAHoliday());
         System.out.println("\n -------------------------------\n"); 
 
         HrDepartment hrd = new HrDepartment();
-        System.out.println(hrd.departmentName());
+        System.out.println(" Welcome to"+ hrd.departmentName());
+        System.out.println(hrd.doActivity());
         System.out.println(hrd.getTodaysWork());
         System.out.println(hrd.getWorkDeadline());
-        System.out.println(hrd.doActivity());
+        System.out.println(hrd.isTodayAHoliday());
         System.out.println("\n -------------------------------\n"); 
 
         TechnicalDepartment tech = new TechnicalDepartment();
-        System.out.println(tech.departmentName());
+        System.out.println(" Welcome to"+ tech.departmentName());  
         System.out.println(tech.getTodaysWork());
-        System.out.println(tech.getWorkDeadline());
+        System.out.println(tech.getWorkDeadline());  
         System.out.println(tech.getTechStackInformation());
+        System.out.println(tech.isTodayAHoliday());
         System.out.println("\n -------------------------------\n"); 
-        
+
     }//End of main method
 }//End of DriverClass
